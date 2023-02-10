c7241425513facceafa02c0360defaf41df76125
Philip Durbin <philipdurbin@gmail.com>
2023-02-09 22:16:30 -0500

    refactor common code
    also run through prettier

--- script.js
+++ script.js
@@ -1,60 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
-    addLinkToPullRequests();
-    addLinkToIssues();
-    document.querySelector("#pull-requests-form").onsubmit = () => {
-        addLinkToPullRequests();
-        return false;
-    };
-    document.querySelector("#issues-form").onsubmit = () => {
-        addLinkToIssues();
-        return false;
-    };
+document.addEventListener("DOMContentLoaded", () => {
+  const repo = "IQSS/dataverse";
+  const prAuthors =
+    "donsizemore,GPortas,landreev,pdurbin,poikilotherm,qqmyers,sekmiller";
+  const issueAuthors =
+    "app/sync-by-unito,djbrooke,dlmurphy,donsizemore,eaquigley,esotiri,ferrys,GPortas,jggautier,kcondon,landreev,mercecrosas,mheppler,mreekie,oscardssmith,pameyer,pdurbin,poikilotherm,posixeleni,qqmyers,raprasad,rtreacy,sbarbosadataverse,scolapasta,sekmiller,suenjedt,TaniaSchlatter";
+  addLink(repo, "pr", prAuthors);
+  addLink(repo, "issue", issueAuthors);
+  document.querySelector("#form-pr").onsubmit = () => {
+    const authors = document.querySelector("#skip-pr").value;
+    addLink(repo, "pr", authors);
+    return false;
+  };
+  document.querySelector("#form-issue").onsubmit = () => {
+    const authors = document.querySelector("#skip-issue").value;
+    addLink(repo, "issue", authors);
+    return false;
+  };
 });
 
-function addLinkToPullRequests() {
-    const orgPlusRepo = document.querySelector('#repoForPullRequests').value;
-    const authors = document.querySelector('#skipForPullRequests').value;
+function addLink(repo, type, authors) {
+  console.log(type);
+  document.querySelector("#repo-" + type).value = repo;
+  document.querySelector("#skip-" + type).value = authors;
+  const baseUrl = "https://github.com/search?s=created&o=desc&state=open&q=";
+  var url = baseUrl + "repo:" + repo + " is:" + type;
+  const authorsToSkip = authors.split(",");
 
-    const baseUrl = "https://github.com/search?s=created&o=desc&state=open&q=";
-    var url = baseUrl + "repo:" + orgPlusRepo + " is:pr";
-    const authorsToSkip = authors.split(",");
+  for (i of authorsToSkip) {
+    url += " -author:" + i;
+  }
 
-    for (i of authorsToSkip) {
-        url += " -author:" + i;
-    }
+  const p = document.createElement("p");
+  p.innerHTML = "Skipping " + authorsToSkip.join(", ");
+  document.querySelector("#results-" + type).append(p);
 
-    const p = document.createElement('p');
-    p.innerHTML = "Skipping " + authorsToSkip.join(", ");
-    document.querySelector('#resultsForPullRequests').append(p);
-
-    var link = document.createElement('a');
-    var linkText = document.createTextNode(url);
-    link.setAttribute('href', url);
-    link.setAttribute('target', '_blank');
-    link.appendChild(linkText);
-    document.querySelector('#resultsForPullRequests').append(link);
-}
-
-function addLinkToIssues() {
-    const orgPlusRepo = document.querySelector('#repoForIssues').value;
-    const authors = document.querySelector('#skipForIssues').value;
-
-    const baseUrl = "https://github.com/search?s=created&o=desc&state=open&q=";
-    var url = baseUrl + "repo:" + orgPlusRepo + " is:issue";
-    const authorsToSkip = authors.split(",");
-
-    for (i of authorsToSkip) {
-        url += " -author:" + i;
-    }
-
-    const p = document.createElement('p');
-    p.innerHTML = "Skipping " + authorsToSkip.join(", ");
-    document.querySelector('#resultsForIssues').append(p);
-
-    var link = document.createElement('a');
-    var linkText = document.createTextNode(url);
-    link.setAttribute('href', url);
-    link.setAttribute('target', '_blank');
-    link.appendChild(linkText);
-    document.querySelector('#resultsForIssues').append(link);
+  var link = document.createElement("a");
+  var linkText = document.createTextNode(url);
+  link.setAttribute("href", url);
+  link.setAttribute("target", "_blank");
+  link.appendChild(linkText);
+  document.querySelector("#results-" + type).append(link);
 }
