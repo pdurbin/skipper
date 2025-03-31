c3f4350ea8647e56581b6212fca12efac0b7f165
Philip Durbin <philipdurbin@gmail.com>
2025-03-31 19:29:57 -0400

    remove DOMContentLoaded, add defer

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const repo = "IQSS/dataverse";
-  const prAuthors =
-    "donsizemore,ErykKul,GPortas,jp-tosca,landreev,pdurbin,poikilotherm,qqmyers,Saixel,sekmiller,stevenwinship";
-  const issueAuthors =
-    "app/sync-by-unito,cmbz,djbrooke,dlmurphy,donsizemore,eaquigley,ErykKul,esotiri,ferrys,GPortas,jggautier,jp-tosca,kcondon,kmika11,landreev,mercecrosas,mheppler,mreekie,oscardssmith,pameyer,pdurbin,poikilotherm,posixeleni,qqmyers,raprasad,rtreacy,Saixel,sbarbosadataverse,scolapasta,sekmiller,stevenwinship,suenjedt,TaniaSchlatter";
-  addLink(repo, "pr", prAuthors);
-  addLink(repo, "issue", issueAuthors);
-  document.querySelector("#form-pr").onsubmit = () => {
-    const repo = document.querySelector("#repo-pr").value;
-    const authors = document.querySelector("#skip-pr").value;
-    addLink(repo, "pr", authors);
-    return false;
-  };
-  document.querySelector("#form-issue").onsubmit = () => {
-    const repo = document.querySelector("#repo-issue").value;
-    const authors = document.querySelector("#skip-issue").value;
-    addLink(repo, "issue", authors);
-    return false;
-  };
-});
+const repo = "IQSS/dataverse";
+const prAuthors =
+  "donsizemore,ErykKul,GPortas,jp-tosca,landreev,pdurbin,poikilotherm,qqmyers,Saixel,sekmiller,stevenwinship";
+const issueAuthors =
+  "app/sync-by-unito,cmbz,djbrooke,dlmurphy,donsizemore,eaquigley,ErykKul,esotiri,ferrys,GPortas,jggautier,jp-tosca,kcondon,kmika11,landreev,mercecrosas,mheppler,mreekie,oscardssmith,pameyer,pdurbin,poikilotherm,posixeleni,qqmyers,raprasad,rtreacy,Saixel,sbarbosadataverse,scolapasta,sekmiller,stevenwinship,suenjedt,TaniaSchlatter";
+addLink(repo, "pr", prAuthors);
+addLink(repo, "issue", issueAuthors);
+document.querySelector("#form-pr").onsubmit = () => {
+  const repo = document.querySelector("#repo-pr").value;
+  const authors = document.querySelector("#skip-pr").value;
+  addLink(repo, "pr", authors);
+  return false;
+};
+document.querySelector("#form-issue").onsubmit = () => {
+  const repo = document.querySelector("#repo-issue").value;
+  const authors = document.querySelector("#skip-issue").value;
+  addLink(repo, "issue", authors);
+  return false;
+};
 
 function addLink(repo, type, authors) {
   console.log(type);
