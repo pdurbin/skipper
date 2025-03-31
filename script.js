b5b35c951fbbd01299cc5e072212fd5d9134afde
Philip Durbin <philipdurbin@gmail.com>
2025-03-31 19:35:43 -0400

    add some vertical space

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const prAuthors =
   "donsizemore,ErykKul,GPortas,jp-tosca,landreev,pdurbin,poikilotherm,qqmyers,Saixel,sekmiller,stevenwinship";
 const issueAuthors =
   "app/sync-by-unito,cmbz,djbrooke,dlmurphy,donsizemore,eaquigley,ErykKul,esotiri,ferrys,GPortas,jggautier,jp-tosca,kcondon,kmika11,landreev,mercecrosas,mheppler,mreekie,oscardssmith,pameyer,pdurbin,poikilotherm,posixeleni,qqmyers,raprasad,rtreacy,Saixel,sbarbosadataverse,scolapasta,sekmiller,stevenwinship,suenjedt,TaniaSchlatter";
+
 addLink(repo, "pr", prAuthors);
 addLink(repo, "issue", issueAuthors);
+
 document.querySelector("#form-pr").onsubmit = () => {
   const repo = document.querySelector("#repo-pr").value;
   const authors = document.querySelector("#skip-pr").value;
   addLink(repo, "pr", authors);
   return false;
 };
+
 document.querySelector("#form-issue").onsubmit = () => {
   const repo = document.querySelector("#repo-issue").value;
   const authors = document.querySelector("#skip-issue").value;
